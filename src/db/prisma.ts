dd583a9afd84ebf751c18213cc60953bc9115f74
Jitendra <jitendraks1803@gmail.com>
2025-12-06 23:31:21 +0530

    use pg adapter instead of neon

--- src/db/prisma.ts
+++ src/db/prisma.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
-import { neonConfig } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from "@/generated/prisma/client";
 
 const connectionString = process.env.DATABASE_URL;
@@ -9,14 +8,7 @@ if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-// Configure WebSocket for Node.js environments (not needed in Edge/Browser)
-if (typeof window === "undefined") {
-  import("ws").then((ws) => {
-    neonConfig.webSocketConstructor = ws.default;
-  });
-}
-
-const adapter = new PrismaNeon({ connectionString });
+const adapter = new PrismaPg({ connectionString });
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -31,4 +23,3 @@ export const prisma =
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
