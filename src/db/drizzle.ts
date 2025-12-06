fba32a912a8c141abf0868393cec84922ea0bd34
Jitendra <jitendraks1803@gmail.com>
2025-12-06 23:57:55 +0530

    use pg adapter instead of neon

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,8 +1,6 @@
 import "dotenv/config";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import { Pool, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
-import ws from "ws";
 
 const connectionString = process.env.DATABASE_URL;
 
@@ -10,10 +8,4 @@ if (!connectionString) {
   throw new Error("DATABASE_URL environment variable is not set");
 }
 
-// Configure WebSocket for Node.js
-neonConfig.webSocketConstructor = ws;
-
-const pool = new Pool({ connectionString });
-
-export const db = drizzle(pool, { schema });
-
+export const db = drizzle(connectionString, { schema });
